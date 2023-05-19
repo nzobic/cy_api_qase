23ec61c599af5c2fdb41e41d389dca6334b69051
nzobic <zobicnatasa@gmail.com>
2023-05-19 15:38:00 +0200

    Updates

--- cypress/e2e/qase-get-request.cy.js
+++ cypress/e2e/qase-get-request.cy.js
@@ -9,7 +9,6 @@ describe('API QASE Tests: GET', () => {
     ).then(response => {
       expect(response.status).eq(200)
       expect(response.body.status).eq(true)
-      // expect(response.body.result.total).eq(5)
       expect(response.body.result.entities[0].title).eq("Demo Project")
       expect(response.body.result.entities[0].code).eq("DEMO")
       expect(response.body.result.entities[0].counts.cases).eq(10)
